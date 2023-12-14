b45a4f48d7954ecd5cfcbca215b4acbe5e34624b
mykola2312 <nikolay_tihonov@i.ua>
2023-12-14 17:28:08 +0200

    add sln parsing

--- main.py
+++ main.py
@@ -10,7 +10,18 @@ if __name__ == "__main__":
     with open(slnPath, "rt") as slnFile:
         sln = slnFile.readlines()
     
+    # find and parse projects
+    for line in sln:
+        if not line.startswith("Project"):
+            continue
+
+        tokens = line.split(" ")
+        projectName = tokens[2][1:-2]
+        projectPath = tokens[3][1:-2]
+
+        print(projectName, projectPath)
+
     # write output sln file
-    slnPath = slnPath.replace("-2005", "")
-    with open(slnPath, "w") as slnFile:
-        slnFile.writelines(sln)
+    # slnPath = slnPath.replace("-2005", "")
+    # with open(slnPath, "w") as slnFile:
+    #     slnFile.writelines(sln)

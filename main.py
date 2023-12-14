5bbb91b5b8d1009ef57a200a7e612349afe596e9
mykola2312 <nikolay_tihonov@i.ua>
2023-12-14 17:20:29 +0200

    sln reading and writing

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import os
+import sys
+
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) < 2:
+        sys.exit("Provide path to 2005 SLN file")
+    
+    # read sln file
+    slnPath = sys.argv[1]
+    with open(slnPath, "rt") as slnFile:
+        sln = slnFile.readlines()
+    
+    # write output sln file
+    slnPath = slnPath.replace("-2005", "")
+    with open(slnPath, "w") as slnFile:
+        slnFile.writelines(sln)
